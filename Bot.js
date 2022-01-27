2c39d699075ffd3a56c867ab655b32487527cc61
Alex Folk <38863228+AlexFolk@users.noreply.github.com>
2022-01-27 12:36:16 -0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,11 @@
+const discord = require('discord.js')
+const fetch = require('node-fetch')
 
+const  client = new Discord.Client()
+
+const tkn = "WZNj1YdW06BBn3ba4-eQKbNyy9SxoyM2"
+
+bot.on("ready", () => {
+  client.login(tkn)
+
+})
